0bd1219303c8edc718d01e8482818c5793ce4b82
Muhammad-Usman08 <usman2037966@gmail.com>
2024-07-17 18:42:27 +0500

    medical app

---
+++ android/app/src/main/kotlin/com/example/medicalapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.medicalapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
